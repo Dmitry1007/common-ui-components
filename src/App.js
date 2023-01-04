60958c3bd8ac711d8ca32ec9960e7c78a912cc5b
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-04 11:28:08 -0700

    Install and configure TailwindCSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <div>
-        <Button primary secondary>Click</Button>
+        <Button primary>Click</Button>
       </div>
       <div>
         <Button>Push</Button>
