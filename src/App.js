42237d5649f7098c7151ec58ce45c888656f8b87
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-04 13:28:46 -0700

    Import react-icons, display icons next to button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import Button from './Button';
+import { GoBell, GoCloudDownload, GoDatabase, GoZap, GoStop } from 'react-icons/go';
 
 function App() {
   return (
     <div>
       <div>
         <Button>
+          <GoBell />
           Plain
         </Button>
       </div>
       <div>
         <Button primary rounded outline>
+          <GoCloudDownload />
           Primary
         </Button>
       </div>
       <div>
         <Button secondary>
+          <GoDatabase />
           Secondary
         </Button>
       </div>
@@ -25,11 +29,13 @@ function App() {
       </div>
       <div>
         <Button warning outline>
+          <GoZap />
           Warning
         </Button>
       </div>
       <div>
         <Button danger rounded>
+          <GoStop />
           Danger
         </Button>
       </div>
