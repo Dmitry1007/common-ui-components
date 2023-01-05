e5c5c7fe9e3f5999a5b779c186db9dd6fa5a18b0
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-04 19:04:13 -0700

    Accordion boilerplate

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,22 @@
+import React from 'react';
+
 function Accordion({ items }) {
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={index}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.label}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
   return (
-    <div>{items[0].label}</div>
+    <div>{renderedItems}</div>
   );
 }
 
