3282718d6e06559a047ce3474d2b255fa05bb25e
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-06 16:48:30 -0700

    Remove the selected color from the list of options

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 
-const colors = ['Selected...', 'Red', 'Green', 'Blue'];
+const colors = ['Red', 'Green', 'Blue'];
 
 function DropDown() {
-  const [selectedColor, setSelectedColor] = useState(colors[0])
+  const [selectedColor, setSelectedColor] = useState('Selected...')
   const [showColors, setShowColors] = useState(false)
 
-  const handleClick = (color) => {
+  const handleSelectColor = (color) => {
     setSelectedColor(color);
     setShowColors(false);
   }
   
-  const renderColors = colors.slice(1).map((color, index) => {
+  const renderColors = colors.filter((color) => {
+    return color !== selectedColor
+  }).map((color, index) => {
     return (
-      <div onClick={() => handleClick(color)} key={index}>{color}</div>
-    )
+      <div onClick={() => handleSelectColor(color)} key={index}>
+        {color}
+      </div>
+    );
   })
 
   return (
