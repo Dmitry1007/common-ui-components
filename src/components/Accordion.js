80e650dcbf3ad2167c176768261cbcf7457232d0
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-06 11:40:22 -0700

    Refactor, create a function that handles a click even

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,6 +5,14 @@ import { GoChevronDown, GoChevronLeft} from 'react-icons/go';
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(-1);
 
+  const handleClick = (index) => {
+    if (index === expandedIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(index);
+    }
+  };
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
@@ -12,7 +20,7 @@ function Accordion({ items }) {
       <div key={item.id}>
         <div
           className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
-          onClick={isExpanded ? () => setExpandedIndex(-1) : () => setExpandedIndex(index)}
+          onClick={() => handleClick(index)}
         >
           {item.label}
           <span className="text-2xl">{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
