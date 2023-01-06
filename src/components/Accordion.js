702d084db21b05632d2e21821f6f8a3871ba5d76
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-06 11:26:04 -0700

    Be able to toggle between expanded and collapsed content on the same item

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { GoChevronDown, GoChevronLeft} from 'react-icons/go';
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
@@ -12,7 +12,7 @@ function Accordion({ items }) {
       <div key={item.id}>
         <div
           className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
-          onClick={() => setExpandedIndex(index)}
+          onClick={isExpanded ? () => setExpandedIndex(-1) : () => setExpandedIndex(index)}
         >
           {item.label}
           <span className="text-2xl">{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
