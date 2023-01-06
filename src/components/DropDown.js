fef76223a75195a6c10d5bae24155bdd25c555b9
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-06 15:27:44 -0700

    Basic Drop down implementation, could be improved

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,7 +1,28 @@
+import { useState } from 'react'
+
+const colors = ['Selected...', 'Red', 'Green', 'Blue'];
+
 function DropDown() {
+  const [selectedColor, setSelectedColor] = useState(colors[0])
+  const [showColors, setShowColors] = useState(false)
+
+  const handleClick = (color) => {
+    setSelectedColor(color);
+    setShowColors(false);
+  }
+  
+  const renderColors = colors.slice(1).map((color, index) => {
+    return (
+      <div onClick={() => handleClick(color)} key={index}>{color}</div>
+    )
+  })
+
   return (
-    <div>Select...</div>
-  )
+    <div>
+      <div onClick={() => setShowColors(!showColors)}>{selectedColor}</div>
+      {showColors ? renderColors : null}
+    </div>
+  );
 }
 
 export default DropDown;
