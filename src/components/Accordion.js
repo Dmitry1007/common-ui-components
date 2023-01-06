8cce8c2d9e15f119328cc21a594ce3e0203bf480
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-06 12:14:11 -0700

    Handle super edge case of state update not happening fast enough, when click events are firing too fast, by grabbing the most current piece of state

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,11 +6,13 @@ function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(-1);
 
   const handleClick = (index) => {
-    if (index === expandedIndex) {
-      setExpandedIndex(-1);
-    } else {
-      setExpandedIndex(index);
-    }
+    setExpandedIndex((currentExpandedIndex) => {
+      if (currentExpandedIndex === index) {
+        return -1;
+      } else {
+        return index;
+      }
+    });
   };
 
   const renderedItems = items.map((item, index) => {
