ff01abf719fd034ccc03e61c326d93015ae7385d
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-07 13:17:41 -0700

    Add some style

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -10,18 +10,23 @@ function DropDown({ options, onChange, value }) {
   
   const renderedOptions = options.map((option, index) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={index}>
+      <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={index}>
         {option.label}
       </div>
     )
   })
 
   return (
-    <div>
-      <div onClick={() => setOpen(!open)}>{ value?.label || 'Select Color' }</div>
-      {open ? renderedOptions : null}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={() => setOpen(!open)}
+      >
+        {value?.label || "Select Color"}
+      </div>
+      {open ? <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div> : null}
     </div>
-  )
+  );
 }
 
 export default DropDown;
