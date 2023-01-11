c4ea5e6f86fcdddee2dd3551220f3d94ffa6aa62
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-10 18:59:18 -0700

    Make modal more reusable, add button inside modal that closes modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,16 @@
 import ReactDom from "react-dom";
 
-function Modal({ onClose }) {
+function Modal({ onClose, children, actionBar }) {
   return ReactDom.createPortal(
     <div>
       <div
         onClick={onClose}
         className="absolute inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">I'm a Dummy modal</div>
+      <div className="absolute inset-40 p-10 bg-white">
+        {children}
+        {actionBar}
+      </div>
     </div>,
     document.querySelector(".modal-container")
   );
