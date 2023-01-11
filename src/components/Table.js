61a72dcd74e36ed500a30ffb733440da3b8cdc07
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-11 15:49:50 -0700

    Make Table component more reusable

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,26 +1,26 @@
 function Table({ data }) {
+  const renderedHeader = Object.keys(data[0]).map((key) => {
+    return <th key={key}>{key}</th>;
+  });
+
+  const renderedRows = data.map((row) => {
+    return (
+      <tr key={row.name}>
+        <td className="border px-4 py-2">{row.name}</td>
+        <td className="border px-4 py-2">
+          <div className={`h-6 w-6 rounded-full ${row.color}`} />
+        </td>
+        <td className="border px-4 py-2">{row.score}</td>
+      </tr>
+    );
+  });
+
   return (
     <table className="table-auto">
       <thead>
-        <tr>
-          <th className="px-4 py-2">Name</th>
-          <th className="px-4 py-2">Color</th>
-          <th className="px-4 py-2">Score</th>
-        </tr>
+        <tr>{renderedHeader}</tr>
       </thead>
-      <tbody>
-        {data.map((item) => {
-          return (
-            <tr key={item.name}>
-              <td className="border px-4 py-2">{item.name}</td>
-              <td className="border px-4 py-2">
-                <div className={`h-6 w-6 rounded-full ${item.color}`} />
-              </td>
-              <td className="border px-4 py-2">{item.score}</td>
-            </tr>
-          );
-        })}
-      </tbody>
+      <tbody>{renderedRows}</tbody>
     </table>
   );
 }
