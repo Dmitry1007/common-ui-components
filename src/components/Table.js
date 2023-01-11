a54e79fe0c856734963f5afd9de1cc26e206f30f
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-11 11:11:55 -0700

    Display Table rows with cute colored circles

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,28 @@
 function Table({ data }) {
-  return <div>{data.length}</div>;
+  return (
+    <table className="table-auto">
+      <thead>
+        <tr>
+          <th className="px-4 py-2">Name</th>
+          <th className="px-4 py-2">Color</th>
+          <th className="px-4 py-2">Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => {
+          return (
+            <tr key={item.name}>
+              <td className="border px-4 py-2">{item.name}</td>
+              <td className="border px-4 py-2">
+                <div className={`h-6 w-6 rounded-full ${item.color}`} />
+              </td>
+              <td className="border px-4 py-2">{item.score}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
