467eca56c6c821013b546a715707c5979cf2b519
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-13 12:35:46 -0700

    Add a sort value to columns in config that need to be sortable

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -10,7 +10,11 @@ function TablePage() {
   ];
 
   const config = [
-    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
     {
       label: "Color",
       render: (fruit) => (
@@ -20,7 +24,7 @@ function TablePage() {
     {
       label: "Score",
       render: (fruit) => fruit.score,
-      header: () => <th className="bg-red-500">Score</th>,
+      sortValue: (fruit) => fruit.score,
     },
   ];
 
