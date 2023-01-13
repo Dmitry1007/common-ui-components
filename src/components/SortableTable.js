87786d7da31784cc58b0623aa67e7299703b54e6
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-13 13:44:21 -0700

    On click of sortable column, set the column name and sort order

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
   const handleClick = (label) => {
-    console.log(label);
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
   };
 
   const updatedConfig = props.config.map((column) => {
