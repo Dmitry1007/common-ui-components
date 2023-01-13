3ab3a1c6f0ea5666e81b4f3e4f38c00d4e4d0af2
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-13 12:59:26 -0700

    add extra property to columns that are sortable

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,21 @@
+import { ViewColumnsIcon } from "@heroicons/react/20/solid";
 import Table from "./Table";
 
 function SortableTable(props) {
-  return <Table {...props} />;
+  const updatedConfig = props.config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => <th>{column.label} IS Sortable</th>,
+    };
+  });
+
+  console.log("updatedConfig", updatedConfig);
+
+  // config will override props.config
+  return <Table {...props} config={updatedConfig} />;
 }
 
 export default SortableTable;
