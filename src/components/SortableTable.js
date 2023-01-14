4678d4d23ccab3d5c6ceebf266a9fcf796e38aaf
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-14 11:51:58 -0700

    Create helper-function/hook for sorting data

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,26 +1,12 @@
-import { useState } from "react";
 import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
 import Table from "./Table";
+import useSort from "../hooks/use-sort";
 
 function SortableTable({ data, config, keyFn }) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-
-  const handleClick = (label) => {
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc" && label === sortBy) {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc" && label === sortBy) {
-      setSortOrder(null);
-      setSortBy(null);
-    } else {
-      setSortOrder("asc");
-      setSortBy(label);
-    }
-  };
+  const { sortOrder, sortBy, sortedData, setSortColumn } = useSort(
+    data,
+    config
+  );
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
@@ -31,7 +17,7 @@ function SortableTable({ data, config, keyFn }) {
       header: () => (
         <th
           className="cursor-pointer hover:bg-gray-100"
-          onClick={() => handleClick(column.label)}
+          onClick={() => setSortColumn(column.label)}
         >
           <div className="flex items-center">
             {getIcons(column.label, sortBy, sortOrder)}
@@ -41,24 +27,6 @@ function SortableTable({ data, config, keyFn }) {
       ),
     };
   });
-
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
 
   const getIcons = (label, sortBy, sortOrder) => {
     if (label !== sortBy) {
