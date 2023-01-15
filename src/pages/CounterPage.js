acab0627c87fab43f815e2dd6301679b9228b0c8
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-15 09:03:07 -0700

    Refactor, add decrement feature to count

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,13 +1,22 @@
-import useCounter from "../hooks/use-counter";
+import { useState } from "react";
 import Button from "../components/Button";
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
   return (
     <div>
       <h2>Count is {count}</h2>
       <Button onClick={increment}>Increment</Button>
+      <Button onClick={decrement}>Increment</Button>
     </div>
   );
 }
