4ce138eb0de6fbb3210c6b2a0782a86d80c2f920
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-15 09:10:43 -0700

    Make a form that takes a number, style it

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "../components/Button";
+import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
@@ -13,11 +14,21 @@ function CounterPage({ initialCount }) {
   };
 
   return (
-    <div>
-      <h2>Count is {count}</h2>
-      <Button onClick={increment}>Increment</Button>
-      <Button onClick={decrement}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h2 className="text-lg">Count is {count}</h2>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Increment</Button>
+      </div>
+      <form>
+        <label>Add a number</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add It!</Button>
+      </form>
+    </Panel>
   );
 }
 
