0191c9a182bea47c4d3e2d7a53a5de8ead8f0ff8
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-15 09:53:26 -0700

    Swap out useState for useReducer, implenent part of useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,33 +1,41 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const reducer = (state, action) => {
+  console.log(state, action);
+};
+
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
 
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
 
   const handleChange = (event) => {
-    const value = parseInt(event.target.value) || 0;
-    setValueToAdd(value);
+    // const value = parseInt(event.target.value) || 0;
+    // setValueToAdd(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <Panel className="m-3">
-      <h2 className="text-lg">Count is {count}</h2>
+      <h2 className="text-lg">Count is {state.count}</h2>
       <div className="flex flex-row">
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Increment</Button>
@@ -38,7 +46,7 @@ function CounterPage({ initialCount }) {
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
           onChange={handleChange}
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
         />
         <Button>Add It!</Button>
       </form>
